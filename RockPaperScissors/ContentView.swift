f21ff8f882db742fa38b58265ff51ecd8d0f0fb9
shomil03 <shomil02020@gmail.com>
2023-09-09 17:15:22 +0530

    basic logic implemented

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,32 +8,130 @@
 import SwiftUI
 
 struct ContentView: View {
+    let images = [ Image(systemName: "scissors"),Image(systemName: "newspaper"),Image(systemName: "cricket.ball.circle")]
+    let win_or_lose = ["Win","Lose"]
+    @State private var computer_choose = Int.random(in: 0...2)
+    @State private var choose_against = Int.random(in: 0...1)
+    let pieces = ["scissors","paper","rock"]
+    @State private var status : Bool = false
+    @State private var showing_alert = false
+    @State private var alert_title = ""
+    @State private var alert_message = ""
+       
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Button("Hello World")
-            {}
-            Form{
-                Section{
-                    Text("Enter Your name")
-                }
-                Spacer()
-                Section{
-                    Button("Madarchod")
-                    {
+            HStack{
+                Text("\(win_or_lose[choose_against]) against :")
+                    .font(.largeTitle.bold())
+                    .padding()
+                images[computer_choose]
+                    .resizable()
+                    .frame(width: 50, height: 50)
+            }
+            VStack{
+                HStack{
+                    ForEach(0..<3){numbers in
+                        Button{
+                            checkinput(numbers)
+                            
+                            
+                        }
+                    label:{
+                        images[numbers]
+                            .resizable()
+                            .frame(width: 70,height: 100)
+                            .padding()
+                    }
+                        
+                        
                         
                     }
-                    Section{
-                        Image("gear")
-                    }
-                    
                 }
             }
+            .frame( maxWidth: .infinity, minHeight: 115)
+            .background(Color.black)
+            .padding()
+            
+            
+            
+
+            
+            
+            
         }
-        .padding()
+        .alert(alert_title, isPresented: $showing_alert){
+            Button("Continue"){
+                playgame()
+            }
+            
+        }message: {
+            Text(alert_message)
+        }
+       
+    }
+    func playgame(){
+        computer_choose = Int.random(in: 0...2)
+        choose_against = Int.random(in: 0...1)
+        
+    }
+    func checkinput(_ input:Int){
+        if(computer_choose==0){
+            if(input == 2){
+                status = true
+            }
+            else{
+                status = false
+            }
+        }
+        if(computer_choose==1){
+            if(input==0){
+                status = true
+            }
+            else{
+                status=false
+            }
+        }
+        if(computer_choose==2){
+            if(input==1){
+                status=true
+            }
+            else{
+                status=false
+            }
+        }
+        if(choose_against == 1){
+            status.toggle()
+        }
+        if(status){
+            alert_title = "You won"
+            if(choose_against == 0){
+                alert_message = "\(pieces[input]) beats \(pieces[computer_choose])"
+                
+            }
+            else{
+                alert_message = "\(pieces[computer_choose]) beats \(pieces[input])"
+            }
+            
+        }
+        else{
+            alert_title = "You lose"
+            if(choose_against == 0){
+                alert_message = "\(pieces[computer_choose]) beats \(pieces[input])"
+                
+            }
+            else{
+               
+                alert_message = "\(pieces[input]) beats \(pieces[computer_choose])"
+            }
+            
+        }
+        if(input == computer_choose){
+            alert_message = "Its a draw!"
+        }
+            
+        showing_alert=true
+        
+        
     }
 }
 
