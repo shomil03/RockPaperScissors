a979e1f564c953cd8e72e050385cebf6a8c5a9b7
shomil03 <shomil02020@gmail.com>
2023-09-09 22:23:59 +0530

    added player and computer score

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -17,19 +17,19 @@ struct ContentView: View {
     @State private var showing_alert = false
     @State private var alert_title = ""
     @State private var alert_message = ""
+    @State private var player_score = 0
+    @State private var computer_score = 0
        
     var body: some View {
         ZStack{
-//        red: 237/255, green: 123/255, blue: 123/255
 
             let color1=Color( red: 131/255, green: 96/255, blue: 150/255, opacity: 1)
             let color2=Color( red: 237/255, green: 123/255, blue: 123/255)
             
             let color3=Color( red: 240/255, green: 184/255, blue: 110/255)
             
-            let color4=Color( red: 235/255, green: 231/255, blue: 108/255)
 
-            RadialGradient(stops: [.init(color: color1, location: 0.30),.init(color: color2, location: 0.30)], center: .top, startRadius: 30, endRadius: 800)
+            RadialGradient(stops: [.init(color: color1, location: 0.30),.init(color: color2, location: 0.30)], center: .top, startRadius: 50, endRadius: 750)
 
             
             VStack {
@@ -45,8 +45,8 @@ struct ContentView: View {
                         .frame(width: 50, height: 50)
                         .foregroundColor(color3)
                 }
-                .padding()
-                .padding()
+                .padding(.top,90)
+                
                 .offset(x: 0, y: -100)
                 VStack(spacing: 30){
                     ForEach(0..<3){numbers in
@@ -73,20 +73,36 @@ struct ContentView: View {
                     
                 }
                 .frame(maxWidth: .infinity)
-                .padding()
+                .padding(.vertical)
                 .background(.ultraThinMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 .padding()
-                .offset(y:-28)
-                HStack{
+                
+                HStack(spacing: 90){
                     VStack{
                         Image(systemName: "figure.run")
+                            .resizable()
+                            .foregroundColor(color1)
+                        Text("\(player_score)")
+                            .font(.title3)
+                            .foregroundColor(.green)
                         
                     }
+                    .frame(width: 40,height: 80)
+//                    .background(Color.black)
                     VStack{
                         Image(systemName: "iphone.gen2")
+                            .resizable()
+                            .foregroundColor(color1)
+                        Text("\(computer_score)")
+                            .font(.title3)
+                            .foregroundColor(.red)
                     }
+                    .frame(width: 40,height: 80)
+//                    .background(Color.black)
+                   
                 }
+                .offset(y:50)
                
                
                 
@@ -144,6 +160,7 @@ struct ContentView: View {
         }
         if(status){
             alert_title = "You won"
+            player_score+=1
             if(choose_against == 0){
                 alert_message = "\(pieces[input]) beats \(pieces[computer_choose])"
                 
@@ -155,6 +172,7 @@ struct ContentView: View {
         }
         else{
             alert_title = "You lose"
+            computer_score+=1
             if(choose_against == 0){
                 alert_message = "\(pieces[computer_choose]) beats \(pieces[input])"
                 
